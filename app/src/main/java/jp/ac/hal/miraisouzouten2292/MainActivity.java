5e55c765073911b8ab0a89703d11b4770c6184fb
matsu-n <53486381+matsu-n@users.noreply.github.com>
2020-10-22 12:56:26 +0900

    aaa

--- app/src/main/java/jp/ac/hal/miraisouzouten2292/MainActivity.java
+++ app/src/main/java/jp/ac/hal/miraisouzouten2292/MainActivity.java
@@ -22,7 +22,7 @@ public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
 
-
+//aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
